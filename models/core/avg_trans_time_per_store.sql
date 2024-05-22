bff9f5de91e1026e748ae74e86ee46580810ad83
Freanu Peria <freanuperia@gmail.com>
2024-05-22 13:54:32 +0000

    updated avg_trans_time_per_store

--- models/core/avg_trans_time_per_store.sql
+++ models/core/avg_trans_time_per_store.sql
@@ -18,69 +18,53 @@ devices AS (
 ),
 
 
-ranked_transactions_device AS (
+transactions_with_device AS (
+
+    -- we need to know how long it takes on average for a store to make five transactions.
     SELECT
-        A.id AS transaction_id
-        ,A.device_id AS device_id
-        ,B.store_id AS store_id
-        ,A.happened_at 
+        A.id
+        ,A.device_id
+        ,B.store_id
+        ,A.happened_at
         ,ROW_NUMBER() OVER(PARTITION BY B.store_id ORDER BY A.happened_at) AS row_num
 
-    FROM
-        transactions A LEFT JOIN devices B
-            ON A.device_id = B.id
-
-    ORDER BY row_num
+    FROM transactions A LEFT JOIN devices B
+        ON A.device_id = B.id
 ),
 
-ranked_transactions_device_next_trans AS (
-    SELECT 
-        A.transaction_id
-        ,A.device_id
-        ,A.store_id
-        ,A.happened_at
-        ,B.happened_at AS next_trans_happened_at
-        ,A.row_num
-
-    FROM
-        ranked_transactions_device A LEFT JOIN ranked_transactions_device B
-            ON A.store_id = B.store_id
-            AND (A.row_num + 1) = B.row_num
-
-),
-
-top_trans_duration_rank AS (
-    SELECT 
-        transaction_id
-        ,device_id
-        ,store_id
+per_store_first_five_trans AS (
+    SELECT
+        store_id
         ,happened_at
-        ,next_trans_happened_at
-        ,DATEDIFF(day, happened_at, next_trans_happened_at) AS trans_duration
         ,row_num
     
     FROM 
-        ranked_transactions_device_next_trans
+        transactions_with_device
+    
+    WHERE row_num <= 5
+),
 
-    WHERE
-        row_num <= 5
-        AND trans_duration IS NOT NULL
-        -- NULL trans_duration will be caused by transactions with no next_trans_happened_at. let's not account for it since there's no other better way to *count* the duration of a transaction
+per_store_to_first_five_trans AS (
+    SELECT    
+        store_id
+        ,MAX(row_num) AS max_row_num
+        ,DATEDIFF(
+            day,
+            MIN(happened_at),
+            MAX(happened_at)
+        ) AS days_to_five_transactions
+
+    FROM
+        per_store_first_five_trans
+    
+    GROUP BY
+        store_id
+    
+    HAVING
+        max_row_num = 5
+    
+
 )
 
+SELECT AVG(days_to_five_transactions) AS average_time_to_five_transactions FROM per_store_to_first_five_trans
 
-
-SELECT
-    store_id
-    ,AVG(trans_duration) AS avg_trans_duration
-    -- weird behavior, won't run if the AVG is not given alias 
-    -- https://stackoverflow.com/questions/56743577/snowflake-sql-compilation-error-missing-column-specification
-    -- https://community.snowflake.com/s/question/0D50Z00008Zg12qSAB/how-do-i-add-currenttimestamp-to-a-view-i-keep-getting-the-error-sql-compilation-error-missing-column-specification
-
-FROM 
-    top_trans_duration_rank
-
-GROUP BY
-    store_id
-
-ORDER BY avg_trans_duration
